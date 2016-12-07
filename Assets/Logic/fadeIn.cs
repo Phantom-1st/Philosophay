3eac003f321f2fcb16e8d4434b7f38323578824d
Phantom-1st <caelum.is.amazing@gmail.com>
2016-12-06 22:35:38 -0700

    yep

---
+++ Assets/Logic/fadeIn.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fadeIn : MonoBehaviour 
+{
+	public float fadeSpeed = 0.8f;
+	private float alpha = 1f;
+
+	void Start()
+	        {
+		
+
+		alpha = 1;
+		Debug.Log("alpha at start: " + alpha);
+
+	          }
+
+
+	void Update()
+	{
+		FromBlack();
+	}
+
+	void FromBlack() 
+	{
+		Renderer rend = GetComponent<Renderer>();
+
+		if (alpha>0)
+		{
+			alpha -= fadeSpeed * Time.deltaTime;
+			Mathf.Clamp01(alpha);
+			Debug.Log("alpha: "+alpha+ " Time: "+Time.deltaTime);
+			rend.material.SetColor("_Color", new Color(0,0,0,alpha));
+		}
+	}
+
+	void OnGUI() 
+	{
+		//if (GUILayout.Button("Press Me"))
+	}
+}
---
+++ Assets/SlideDoor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (j<1)
+		{
+			if (Left)
+			transform.Translate(Vector3.left*Time.deltaTime);
+			else
+			transform.Translate(Vector3.right*Time.deltaTime);
+
+			
+			j+=0.01f;
+		}
+	}
+
+	float j = 0;
+	public bool Left;
+}
